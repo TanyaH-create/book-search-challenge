bfc5d4d9c75b093e3778200bcb963a5bd0ae72cb
Tanya <dougtanyah@gmail.com>
2025-03-06 17:36:13 -0500

    debug: Book schema and function mismatched parms fixed

--- client/src/pages/SavedBooks.tsx
+++ client/src/pages/SavedBooks.tsx
@@ -18,40 +18,8 @@ const SavedBooks = () => {
   // Extract userData from the query result
   const userData = data?.me || {};
 
-  // const [userData, setUserData] = useState<User>({
-  //   username: '',
-  //   email: '',
-  //   password: '',
-  //   savedBooks: [],
-  // });
-
-  // use this to determine if `useEffect()` hook needs to run again
-  // const userDataLength = Object.keys(userData).length;
-
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong!');
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userDataLength]);
+  
+  
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId: string) => {
@@ -106,10 +74,10 @@ const SavedBooks = () => {
             : 'You have no saved books!'}
         </h2>
         <Row>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks.map((book: any) => {
             return (
-              <Col md='4'>
-                <Card key={book.bookId} border='dark'>
+              <Col md='4' key={book.bookId}>
+                <Card  border='dark'>
                   {book.image ? (
                     <Card.Img
                       src={book.image}
