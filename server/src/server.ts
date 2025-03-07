379fce411a667d94a1263717dc28fea82be6945b
Tanya <dougtanyah@gmail.com>
2025-03-07 14:57:46 -0500

    Fix: server.ts production load files path

--- server/src/server.ts
+++ server/src/server.ts
@@ -48,9 +48,9 @@ const errorHandler: ErrorRequestHandler = (err, _req: Request, res: Response, _n
 app.use(errorHandler);
 
 
-  // if we're in production, serve client/dist as static assets
+  // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (_req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
